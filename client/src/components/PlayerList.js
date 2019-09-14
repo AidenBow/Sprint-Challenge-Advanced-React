9a48767338cea1c0d2cb53f6b671ed4f540d4fa3
AidenBow <AidenBow@users.noreply.github.com>
2019-09-13 18:14:34 -0700

    created playerList component boilerplate

---
+++ client/src/components/PlayerList.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+
+class PlayerList extends Component {
+    constructor(){
+        super();
+        this.state = {
+            players: []
+        }
+    }
+
+    
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
