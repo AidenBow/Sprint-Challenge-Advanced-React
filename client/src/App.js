eb3ce2de892acfe2409f2d24944eb55c601f4485
AidenBow <AidenBow@users.noreply.github.com>
2019-09-13 19:07:20 -0700

    added darkmode custom hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import PlayerList from "./components/PlayerList"
+import useDarkMode from "./hooks/useDarkMode"
+
 function App() {
+
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggle = () => {
+    setDarkMode(!darkMode)
+  }
+
   return (
     <div className="App">
-      <p>dawadwd</p>
+      <button onClick={ () => toggle() }>Dark Mode</button>
       <PlayerList />
     </div>
   );
---
+++ client/src/hooks/useDarkMode.js
@@ -0,0 +1,18 @@
+import {useEffect, useState} from "react"
+
+export const useDarkMode = () => {
+    const [darkValue, setDarkValue] = useState()
+
+    useEffect(() => {
+        const body = document.querySelector('body');
+        if (darkValue) {
+            body.classList.add('darkmode')
+        } else {
+            body.classList.remove('darkmode')
+        }
+    }, [darkValue])
+
+    return [darkValue, setDarkValue]
+}
+
+export default useDarkMode
